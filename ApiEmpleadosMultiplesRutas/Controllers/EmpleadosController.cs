8cbadb169b5d1aeb2af8f44945d6f5bcc649a96f
Danirolan21 <danirolan21@hotmail.com>
2025-03-24 10:29:24 +0100

    Add project files.

---
+++ ApiEmpleadosMultiplesRutas/Controllers/EmpleadosController.cs
@@ -0,0 +1,51 @@
+﻿using ApiEmpleadosMultiplesRutas.Models;
+using ApiEmpleadosMultiplesRutas.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiEmpleadosMultiplesRutas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpleadosController : ControllerBase
+    {
+        private RepositoryEmpleados repo;
+
+        public EmpleadosController(RepositoryEmpleados repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Empleado>>>
+            GetEmpleados()
+        {
+            return await this.repo.GetEmpleadosAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Empleado>> FindEmpleado
+            (int id)
+        {
+            return await this.repo.FindEmpleadoAsync(id);
+        }
+
+        [HttpGet]
+        [Route("[action]/{oficio}")]
+        public async Task<ActionResult<List<Empleado>>>
+            GetEmpleadosOficios(string oficio)
+        {
+            return await this.repo.GetEmpleadosOficioAsync(oficio);
+        }
+
+        [HttpGet]
+        [Route("[action]/{salario}/{iddepartamento}")]
+        public async Task<ActionResult<List<Empleado>>>
+            EmpleadosSalarioDepartamento(int salario,
+            int iddepartamento)
+        {
+            return await this.repo
+                .GetEmpleadosSalarioAsync(salario, iddepartamento);
+        }
+    }
+}
